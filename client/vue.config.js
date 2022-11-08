3633a79abea3f808d1876a12ab404a2d22c09985
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-11-08 21:30:24 +0700

    build: init

---
+++ client/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    transpileDependencies: true,
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+            },
+        },
+    },
+}
